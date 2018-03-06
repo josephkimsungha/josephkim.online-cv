3feef63276d68f2c53fd10e3d3240c70e933e18f
Joseph Kim <joseph.kim.sungha@gmail.com>
2018-03-06 18:27:14 +1100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    Welcome to Joseph Kim's website! Please hold as we undergo construction <span><img alt="ðŸ™‚" class="_1ift _2560 img" src="https://static.xx.fbcdn.net/images/emoji.php/v9/z4c/1/16/1f642.png" /></span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Assistant', Helvetica, Arial, sans-serif;
+  font-weight: 200;
+  text-align: center;
+}
+</style>
