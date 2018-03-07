971d4bd6ef44c341d91dc6c64e702358f2237d49
Joseph Kim <joseph.kim.sungha@gmail.com>
2018-03-08 05:30:33 +1100

    Created a menu header

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,41 @@
-<template>
-  <div id="app">
-    Welcome to Joseph Kim's website! Please hold as we undergo construction <span><img alt="ðŸ™‚" class="_1ift _2560 img" src="https://static.xx.fbcdn.net/images/emoji.php/v9/z4c/1/16/1f642.png" /></span>
-  </div>
-</template>
-
 <script>
+import MainHeader from "./components/MainHeader"
+
+const navLinks = [
+  {
+    label: "Home"
+  },
+  {
+    label: "CV"
+  }
+]
+
 export default {
-  name: 'App'
+  name: 'App',
+  data: function() {
+    return {
+      navLinks
+    }
+  },
+  components: {
+    "main-header": MainHeader
+  }
 }
 </script>
+
+<template>
+  <div id="app">
+    <main-header :navLinks="navLinks" />
+    Welcome to Joseph Kim's website!!! Please hold as we undergo construction <span><img alt="ðŸ™‚" class="_1ift _2560 img" src="https://static.xx.fbcdn.net/images/emoji.php/v9/z4c/1/16/1f642.png" /></span>
+    <div v-for="_ in Array(100)">
+      Lorem Ipsum
+    </div>
+  </div>
+</template>
 
 <style>
 #app {
   font-family: 'Assistant', Helvetica, Arial, sans-serif;
   font-weight: 200;
-  text-align: center;
 }
 </style>
---
+++ src/components/MainHeader.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  name: 'MainHeader',
+  props: ["navLinks"]
+}
+</script>
+
+<template>
+  <div id="main-header">
+    <div class="ui basic inverted segment">
+      <div class="ui container">
+        <div class="ui inverted secondary pointing menu">
+          <a v-for="link of navLinks" class="item">{{ link.label }}</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+#main-header {
+}
+</style>
