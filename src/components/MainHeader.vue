82dd0ffac884aa918a16fa7f29393705c0167605
Joseph Kim <joseph.kim.sungha@gmail.com>
2018-03-08 08:21:54 +1100

    Created secondary menu header which will show when scrolling down

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,23 +1,48 @@
 <script>
 export default {
   name: 'MainHeader',
-  props: ["navLinks"]
+  props: ["navLinks"],
+  data: function() {
+    return {
+      fixed: false
+    }
+  },
+  methods: {
+    fixMenu(isVisible, entry) {
+      this.fixed = !isVisible
+    }
+  }
 }
 </script>
 
 <template>
   <div id="main-header">
-    <div class="ui basic inverted segment">
+    <div class="ui basic inverted segment header">
       <div class="ui container">
-        <div class="ui inverted secondary pointing menu">
+        <div class="ui inverted secondary pointing menu" v-observe-visibility="fixMenu">
           <a v-for="link of navLinks" class="item">{{ link.label }}</a>
         </div>
       </div>
     </div>
+
+    <transition
+      name="custom-classes-transition"
+      enter-active-class="animated fadeInDown"
+      leave-active-class="animated fadeOutUp"
+    >
+      <div v-if="fixed" class="ui menu fixed">
+        <div class="ui container">
+          <a v-for="link of navLinks" class="item">{{ link.label }}</a>
+        </div>
+      </div>
+    </transition>
   </div>
 </template>
 
 <style>
-#main-header {
+#main-header .header {
+  padding-top: 0;
+  padding-bottom: 0;
+  margin-bottom: 1em;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VueObserveVisibility from 'vue-observe-visibility'
 import App from './App'
 
 Vue.config.productionTip = false
+Vue.use(VueObserveVisibility)
 
 /* eslint-disable no-new */
 new Vue({
