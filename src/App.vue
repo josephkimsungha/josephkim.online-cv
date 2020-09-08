1cfbe27704dd7f2ab57eba0f60bcb336c452815d
Joseph Kim <joseph.kim.sungha@gmail.com>
2020-09-08 22:05:33 +1000

    Copied over (and updated) content from previous site.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,12 @@
 <template>
   <div id="app">
-    <v-button
-      text="wow"
-      link="/"
-    />
-    <v-button
-      text="two"
-      link="/two"
-    />
-    <v-button
-      text="three"
-    />
     <router-view />
   </div>
 </template>
 
 <script>
-import Button from "./components/Button";
-
 export default {
   name: "App",
-  components: {
-    "v-button": Button,
-  },
 };
 </script>
 
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,6 +1,228 @@
 <template>
   <div id="home">
-    Hello world
+    <h1>Under (re)construction</h1>
+    In the meantime, here's a summary of relevant info in boring ol' text.
+
+    <h2>Employment history</h2>
+    <h3>Software engineer at Google</h3>
+    <small>January 2020 - Present</small>
+    <div>How fun!</div>
+    <h3>Software developer at Jarvis Home Services </h3>
+    <small>June 2018 - June 2019</small>
+    <div>
+      Working as a developer for a start-up company taught me how to learn
+      quickly on the job and to focus on more than just my own projects but to
+      maintain consideration of the benefit of the company as a whole. I began
+      working on a full-time basis since November 2018.
+    </div>
+    <h4>Skills gained</h4>
+    <h5>Front end</h5>
+    <ul>
+      <li>React/Redux</li>
+      <li>Styling in Semantic UI</li>
+      <li>GraphQL</li>
+      <li>Bindings for various backend technologies (Feathers JS, Prisma)</li>
+    </ul>
+    <h5>Back end</h5>
+    <ul>
+      <li>Express</li>
+      <li>Feathers JS</li>
+      <li>Prisma</li>
+      <li>Zendesk/Google Sheet APIs</li>
+    </ul>
+    <h5>Other</h5>
+    <ul>
+      <li>Docker</li>
+      <li>Gitlab CI/CD</li>
+      <li>Rancher Deployment</li>
+      <li>Salesforce</li>
+      <li>Zapier App</li>
+    </ul>
+    <h3>Software developer at Telstra</h3>
+    <small>July - December 2017</small>
+    <div>
+      Working in an Agile team of software developers at Telstra provided
+      opportunity to grow in full-stack development.
+    </div>
+    <h5>Major project 1</h5>
+    <ul>
+      <li>Mainly front-end development</li>
+      <li>Styling in Bootstrap and LESS</li>
+      <li>SPA navigation using React Routercker</li>
+    </ul>
+    <h5>Major project 2</h5>
+    <ul>
+      <li>Mainly back-end development</li>
+      <li>Creation of API server in Express</li>
+      <li>Communication with 3rd party XPath APIs</li>
+      <li>State management using Redux</li>
+    </ul>
+    <h3>Classroom tutor at Edu Kingdom College</h3>
+    <small>January 2015 - December 2018</small>
+    <div>
+      While working as a tutor I have learnt to balance keeping the classroom a
+      fun and engaging environment and remaining firm to ensure all students
+      have the same opportunity to learn unimpeded.
+    </div>
+
+    <h2>Personal Projects</h2>
+    <h3>Student Study Spaces</h3>
+    <small>
+      React, Redux, Firebase (Hosting, Database, Functions), Semantic UI
+    </small>
+    <div>
+      This project was my team's final submission for Facebook Melbourne Hack
+      2018. We worked in a team of 4 to produce an application for students to
+      be able to find others in their class and collaborate in real time on
+      study notes, chat and create shared quizzes.
+    </div>
+    <div>
+      This application placed in 3<sup>rd</sup>, and a slowly degrading remnant
+      of the demo can be found
+      <a href="https://student-study-space.firebaseapp.com/">here</a>.
+    </div>
+    <h3>Australian New Goldberg Orchestra Main Website</h3>
+    <small>
+      Replicated individually in both React and Vue, Firebase (Database,
+      Functions), Semantic UI
+    </small>
+    <div>
+      The ANGO website provides a gateway for members of the orchesta to find
+      relevant information relating to upcoming concerts and rehearsals. There
+      is a login for members to access rehearsal parts and the rehearsal
+      schedule, and logging in with admin credentials allows the user to reset
+      passwords, edit concert details and create rehearsals. I was also involved
+      in creating some of the designs through Adobe Illustrator.
+    </div>
+    <div>
+      The current version of the website can be found
+      <a href="http://ango.org.au/">here</a>.
+    </div>
+    <h3>This website</h3>
+    <small>Vue</small>
+    <div>Eh, it's a bit of a mess right now. Let's not talk about it.</div>
+
+    <h2>Volunteering</h2>
+    <h3>Webmaster at the Australian New Goldberg Orchestra</h3>
+    <small>January - December 2019</small>
+    <div>Maintained and added new features to the orchestra's website.</div>
+    <h3>Marketing Officer at the Australian New Goldberg Orchestra</h3>
+    <small>January 2018 - January 2019</small>
+    <div>
+      Responsible for creating designs for concert material such as promotional
+      posters and programmes, as well as the developing/maintaining the
+      orchestra's website.
+    </div>
+    <h3>Worship Leader at New Life Evangelical Church</h3>
+    <small>March 2012 - December 2019</small>
+    <div>
+      Learnt how to lead a team, including being clear in direction and
+      listening to the opinons of others. Required juggling many considerations
+      such as time constraints, effective execution of purpose, engagement, etc.
+    </div>
+    <h3>Secretary at the Australian New Goldberg Orchestra</h3>
+    <small>August 2016 - January 2018</small>
+    <div>
+      Compiled meeting agendas and took minutes. Learnt how to work in a
+      committee and make important decisions regarding the future of an
+      organisation.
+    </div>
+    <h3>Service Coordinator at New Life Evangelical Church</h3>
+    <small>January 2012 - January 2017</small>
+    <div>
+      Created church service plans and kept key participants informed on a
+      weekly basis. Trained ten interns, with a maximum of six at one time.
+    </div>
+
+    <h2>Education</h2>
+    <h3>Bachelor of Science and Computer Science</h3>
+    <small>
+      February 2015 - November 2019, Monash University (Clayton Campus)
+    </small>
+    <ul>
+      <li>Extended major in Psychology, major in Computer Science</li>
+      <li>Recognised with an “Excellence in IT” scholarship</li>
+      <li>Final 4.0 Grade Point Average: 3.516</li>
+      <li>Final Weighted Average Mark: 80.556</li>
+    </ul>
+    <h3>Victorian Certificate of Education (VCE)</h3>
+    <small>January 2013 - November 2014, Camberwell Grammar School</small>
+    <ul>
+      <li>ATAR: 98.90</li>
+      <li>
+        Completed VCE subjects:
+        <ul>
+          <li>English 3/4</li>
+          <li>Mathematical Methods 3/4</li>
+          <li>Specialist Mathematics</li>
+          <li>Physics 3/4</li>
+          <li>Chinese as a Second Language 3/4</li>
+        </ul>
+      </li>
+      <li>Cisco networking VET subject completed</li>
+      <li>
+        University Mathematics (MAST10013) completed through Melbourne
+        University
+      </li>
+    </ul>
+
+    <h2>Skills</h2>
+    <ul>
+      <li>Proficiency in Python (formal education)</li>
+      <li>
+        Proficiency in Javascript, including the following frameworks for
+        full-stack development:
+        <ul>
+          <li>React</li>
+          <li>Redux</li>
+          <li>Webpack</li>
+          <li>Vue</li>
+          <li>jQuery</li>
+          <li>Node.js</li>
+          <li>Express</li>
+          <li>Socket.IO</li>
+        </ul>
+      </li>
+      <li>Styling in Bootstrap, Material Design and Semantic UI</li>
+      <li>Solid grasp of Git and Bash commands</li>
+      <li>Self-taught C++ to working standard</li>
+      <li>Experience in SQL</li>
+      <li>
+        Ability to work in teams and independently
+        <ul>
+          <li>Worked in an Agile team at Telstra</li>
+          <li>
+            Participated in a group research project in IT exploring Augmented
+            Reality Sandbox software
+          </li>
+          <li>
+            Picked up front-end development independently through Udemy and
+            framework documentation
+          </li>
+        </ul>
+      </li>
+    </ul>
+
+    <h2>Achievements</h2>
+    <ul>
+      <li>3<sup>rd</sup> place at Facebook Melbourne Hack 2018</li>
+      <li>Placed within top 10% for the IEEEXtreme 10.0</li>
+      <li>Placed within top 11% for the IEEEXtreme 11.0</li>
+      <li>Distinction in Rio Tinto Science Competition</li>
+      <li>
+        High distinctions in various University of Melbourne competitions/ICAS
+        competitions
+      </li>
+      <li>Attained AMEB grade 8 Violin</li>
+    </ul>
+
+    <h2>Referees</h2>
+    <h3>Mr C. Riley</h3>
+    <small>Software Engineering Technology Manager at Telstra</small>
+    <div>Christopher.Riley@team.telstra.com</div>
+    <h3>Ms S. Koo</h3>
+    <small>Manager at Edu Kingdom College Box Hill</small>
+    <div>suzie@i-ekc.com</div>
   </div>
 </template>
 
